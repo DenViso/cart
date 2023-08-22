1ab4cec8c1497529529932ba0701a27bb424554e
DenViso <itdeniro00@gmail.com>
2023-08-22 09:26:12 +0300

    add gh

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: 'http://denviso.github.io/cart',
+    base: './',
   }
 
   if (command !== 'serve') {
